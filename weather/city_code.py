e377c16ac36c7a9a7005894e0b63c7337340274b
Yan Jiang <yjiang@eee168.com>
2014-08-21 16:17:10 +0800

    城市映射表独立为单独的文件

---
+++ weather/city_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*-coding:utf8-*-
+city_code = {
+    'beijing': {'code':54511, 'name':u'北京'},
+    'chengdu': {'code':56294, 'name':u'成都'},
+    'hangzhou': {'code':58457, 'name':u'杭州'},
+    'changsha': {'code':57687, 'name':u'长沙'},
+    'wuhan': {'code':57494, 'name':u'武汉'},
+    'fuzhou':{'code':58847, 'name':u'福州'},
+    'ningbo':{'code':58465, 'name':u'宁波'},
+    'shaoyang':{'code':57766,'name':u'邵阳'},
+    'zibo':{'code':54830, 'name':u'淄博'},
+    'linzi':{'code':60815, 'name':u'临淄'}
+}
+
--- weather/weather.py
+++ weather/weather.py
@@ -6,23 +6,13 @@ import time
 import json
 import sys
 import smtplib
+import city_code
 from email.mime.text import MIMEText
 from email.Header import Header
 
 class weather():
     def __init__(self):
-        self.city_code = {
-            'beijing': {'code':54511, 'name':u'北京'},
-            'chengdu': {'code':56294, 'name':u'成都'},
-            'hangzhou': {'code':58457, 'name':u'杭州'},
-            'changsha': {'code':57687, 'name':u'长沙'},
-            'wuhan': {'code':57494, 'name':u'武汉'},
-            'fuzhou':{'code':58847, 'name':u'福州'},
-            'ningbo':{'code':58465, 'name':u'宁波'},
-            'shaoyang':{'code':57766,'name':u'邵阳'},
-            'zibo':{'code':54830, 'name':u'淄博'},
-            'linzi':{'code':60815, 'name':u'临淄'}
-        }
+        self.city_code = city_code
 
     def get_weather(self, city):
         base_url = "http://tianqi.2345.com"
